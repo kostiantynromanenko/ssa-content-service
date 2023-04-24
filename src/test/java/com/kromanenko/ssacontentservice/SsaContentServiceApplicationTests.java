d9c9afc818e04055d51a82998f05b2b4826c2eeb
Konstantin Romanenko <kostiantyn.romanenko@gmail.com>
2023-04-24 11:18:29 +0300

    init commit

---
+++ src/test/java/com/kromanenko/ssacontentservice/SsaContentServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kromanenko.ssacontentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsaContentServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
